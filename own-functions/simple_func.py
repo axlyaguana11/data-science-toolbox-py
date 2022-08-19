d79ae778592fb0bba9a656994e4baf7f1aef8f89
Axel Yaguana <axl9809@gamil.com>
2022-08-19 14:55:55 +0000

    First commit

---
+++ own-functions/simple_func.py
@@ -0,0 +1,8 @@
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    
+
+    # Print shout_word
+    print('congratulations'+'!!!')
